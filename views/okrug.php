b7ad95214abf7ca9a9e398d19ccf1de970507698
dule1703 <duledrljaca@yahoo.com>
2024-02-28 18:37:23 +0100

    Change git ignore file and okrug.php

--- views/okrug.php
+++ views/okrug.php
@@ -29,10 +29,7 @@ include './header.php';
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
                 <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">КРЕИРАЊЕ ОКРУГА</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="update-tab"  data-bs-toggle="tab" href="#update" role="tab" aria-controls="update" aria-selected="false">АЖУРИРАЊЕ ОКРУГА</a>
-            </li>
+            </li>            
             <li class="nav-item" role="presentation">
                 <a class="nav-link" id="delete-tab"  data-bs-toggle="tab" href="#delete" role="tab" aria-controls="delete" aria-selected="false">БРИСАЊЕ ОКРУГА</a>
             </li> 
@@ -107,74 +104,7 @@ include './header.php';
 
             </div>
 
-
-            <!-- АЖУРИРАЊЕ ПОДАТАКА -->
-            <div class="tab-pane fade" id="update" role="tabpanel" aria-labelledby="update-tab">               
-                <div class="row justify-content-center">                
-                    <div class="col-lg-6 col-xs-12">                      
-                        <form  id="updateOkrugForm" >  
-
-                            <!-- Унос назива округа -->
-                            <div class="form-group mt-5">
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4  d-flex align-items-center justify-content-sm-end justify-content-center">
-                                        <label class="title-color" for="naziv_okruga">Округ:</label>
-                                    </div>                                   
-                                    <div class="col-lg-8 col-sm-8">
-                                        <input  class="form-control form_data" id="naziv_okruga" name="naziv_okruga" type="text" placeholder="Унесите назив округа..." pattern="^[A-Za-zА-Яа-яЁёЉљЊњЂђЋћЧчЏџЈјĐđŽžČčĆć\s]+$"   title="Можете користити само слова и празнине" required>     
-                                    </div>                                     
-                                </div>
-                            </div> 
-
-                            <!-- Повереништво -->                        
-                            <div class="form-group mt-2">
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4  d-flex align-items-center justify-content-sm-end justify-content-center">
-                                        <label class="title-color" for="poverenistvoAdminOkrug">Повереништва:</label>
-                                    </div>
-                                    <div class="col-lg-8 col-sm-8">
-                                        <select  id="poverenistvoAdminOkrug" name="poverenistvoAdminOkrug" multiple class="form-control form_data mt-2">
-
-                                        </select>                                          
-                                    </div>
-
-                                </div>
-                                <div class="row mt-3">
-                                    <div class="col-lg-4 col-sm-4  d-flex">
-
-                                    </div>
-                                    <div class="col-lg-8 col-sm-8">
-                                        <span id="output" class="mt-2"></span>
-                                    </div>
-                                </div>
-                            </div>                          
-                            <div id="messagePoverenistvoUnos" style="text-align: center"></div>
-                            <!-- Дугме за потврду уноса -->
-                            <div class="btn_tabela_sep">
-                                <button  type="submit" id="submitPoverenistvo" class="btn btn-primary mt-5 mb-2"  >Потврди унос</button> 
-                                <hr class="border border-primary form_separator">
-                            </div>
-                        </form>
-                        <div class="modal fade" id="confirmationModalPoverenistvo" tabindex="-1" aria-labelledby="confirmationModalPoverenistvo" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="confirmationModalPoverenistvo">Потврда</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Да ли сте сигурни да желите ове податке унети?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" id="confirmButtonPov" class="btn btn-primary">Потврди</button>
-                                        <button type="button" id="rejectButtonPov" class="btn btn-secondary" data-bs-dismiss="modal">Затвори</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>   
-            </div>
+ 
             <!-- БРИСАЊЕ ПОДАТАКА -->
             <div class="tab-pane fade" id="delete" role="tabpanel" aria-labelledby="delete-tab">               
                 <div class="row justify-content-center">                
